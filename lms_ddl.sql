c845870a9e5c27fefae970e834c24fc3122c7463
bitoogii <bitoogii@yahoo.com>
2024-05-03 22:14:44 -0500

    Update lms_ddl.sql
    changes on nulls

--- lms_ddl.sql
+++ lms_ddl.sql
@@ -18,14 +18,14 @@ create table course_session
 (
     course_session_id int         not null
         primary key,
-    instructor_id     int         null,
+    instructor_id     int         not null,
     start_date        date        null,
     end_date          date        null,
     days_time         varchar(20) null, -- ‘MWF 9am-9.50am’ or ‘TR 3.30pm-5.20pm’
     course_id         int         null,
     constraint course_session_course_course_id_fk
         foreign key (course_id) references course (course_id),
-    constraint instructor_section___fk
+    constraint instructor_section_fk
         foreign key (instructor_id) references instuctor (instructor_id)
 );
 
@@ -33,10 +33,10 @@ create table assignment
 (
     assignment_id int      not null
         primary key,
-    course_id     int      null,
-    start_date    datetime null,
-    due_date      datetime null,
-    constraint course_assignment___fk
+    course_id     int      not null,
+    start_date    datetime not null,
+    due_date      datetime not null,
+    constraint course_assignment_fk
         foreign key (course_id) references course_session (course_session_id)
 );
 
@@ -56,8 +56,8 @@ create table student_course_enrollment
 (
     student_course_id int auto_increment
         primary key,
-    student_id        int  null,
-    course_id         int  null,
+    student_id        int  not null,
+    course_id         int  not null,
     grade_point       int  null, -- 0~100
     grade             char null, -- A,B,C,D,F
     constraint student_course_enrollment_course_id_fk
@@ -70,17 +70,17 @@ create table assignment_submission
 (
     submission_id     int           not null
         primary key,
-    student_course_id int           null,
-    assignment_id     int           null,
+    student_course_id int           not null,
+    assignment_id     int           not null,
     submission        varchar(255)  null,
     submit_date       datetime      null, -- Submitted date only
     status            int default 0 null, -- 0: Not Started, 1: Started, 2: Submitted
     grading_status    int default 0 null, -- 0: Not started Grading, 1: Started Grading, 2: Completed Grading, 3: Sent to Students
     grade_point       int default 0 null, -- 0~100
     grade             char          null, -- A,B,C,D,F
-    constraint assignment_course_student_assignment___fk
+    constraint assignment_course_student_assignment_fk
         foreign key (assignment_id) references assignment (assignment_id),
-    constraint sce_course_student_assignment___fk
+    constraint sci_course_student_assignment_fk
         foreign key (student_course_id) references student_course_enrollment (student_course_id)
 );
 
